a51ac5cb7f83cae262e0810883fee36a85eabb44
2405-011-pw <walypa56@gmail.com>
2024-07-11 15:40:56 +0100

    upgrade post function

--- src/index.js
+++ src/index.js
@@ -42,13 +42,27 @@ app.get('/api/notes/:id', (request, response) => {
     response.status(204).end()
   })
 
-  app.post('/api/notes', (request, response) => {
+  const generateId = () => {
     const maxId = notes.length > 0
-      ? Math.max(...notes.map(n => Number(n.id))) 
+      ? Math.max(...notes.map(n => Number(n.id)))
       : 0
+    return String(maxId + 1)
+  }
   
-    const note = request.body
-    note.id = String(maxId + 1)
+  app.post('/api/notes', (request, response) => {
+    const body = request.body
+  
+    if (!body.content) {
+      return response.status(400).json({ 
+        error: 'content missing' 
+      })
+    }
+  
+    const note = {
+      content: body.content,
+      important: Boolean(body.important) || false,
+      id: generateId(),
+    }
   
     notes = notes.concat(note)
   
