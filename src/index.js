0022b8721a5e4bff67dd11437f8100419ab8f96b
2405-011-pw <walypa56@gmail.com>
2024-07-11 15:14:00 +0100

    get and delete test

--- src/index.js
+++ src/index.js
@@ -23,9 +23,22 @@ app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/notes', (request, response) => {
-  response.json(notes)
-})
+app.get('/api/notes/:id', (request, response) => {
+    const id = request.params.id
+    const note = notes.find(note => note.id == id)
+    if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+  })
+
+  app.delete('/api/notes/:id', (request, response) => {
+    const id = request.params.id
+    notes = notes.filter(note => note.id != id)
+  
+    response.status(204).end()
+  })
 
 const PORT = 3001
 app.listen(PORT, () => {
